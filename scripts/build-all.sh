7c2bb524c36b25464412e547374949357045dc55
buehlera <anian.buehler@reutlingen-university.de>
2026-08-05 18:50:02 +0200

    updated build-all.sh

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -14,4 +14,4 @@ podman build -t oc-proxy -f proxy/Dockerfile proxy/
 echo "==> Fertig. Proxy starten mit:"
 echo "    podman run -d --name oc-proxy -p 127.0.0.1:3128:3128 oc-proxy"
 echo "Sandbox starten mit:"
-echo "    scripts/start.sh <projekt-root> [--use_proxy] [--offline] [--hil_mode]"
+echo "    scripts/start.sh <projekt-root> [--use_proxy] [--offline] [--hil_mode] [--cbm_ui]"
